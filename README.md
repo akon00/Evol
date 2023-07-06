d527e7f5dcc9e4d01d56982e39d7776ddcff32cd
Mr.Knight <109825410+akon00@users.noreply.github.com>
2023-07-07 03:27:16 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Evol
+<h1 align="center">Welcome to Ducko's V14 (Full Bot)</h1>
+<h3 align="center">Bot made with 💖 by Ducko#7068</h3>
+
+<div align="center">
+ 
+[![discord](https://img.shields.io/discord/909261119103832084?style=for-the-badge&color=5865f2&label=Discord)](https://discord.gg/TKz7BMwEap)
+
+[![issues](https://img.shields.io/github/issues/DuckoDas/DJS-Suggestion-System-v14?style=for-the-badge&color=d84559)](https://github.com/DuckoDas/DJS-Suggestion-System-v14)
+[![stars](https://img.shields.io/github/stars/DuckoDas/DJS-Suggestion-System-v14?color=009F81&label=stars&style=for-the-badge)](https://github.com/DuckoDas/DJS-Suggestion-System-v14)
+[![followers](https://img.shields.io/github/followers/DuckoDas?color=009F81&style=for-the-badge)](https://github.com/DuckoDas/)
+
+**Advanced Suggestions System with Modals and Buttons**
+
+**Upvote and Downvote button**
+
+**Role to accept or decline or restart a suggestion!**
+
+**Full Ticket System Included**
+
+</div>
+<hr>
+
+## Features
+- Custom colors for default the embed, accepted suggestions, and declined suggestions (Setup)
+- Accept or decline or just start the suggestion again (Commands)
+- Upvote or downvote a suggestion
+- Add and remove people from tickets
+- Create tickets and close or archive them!
+
+## **Dependencies:**
+- NodeJS
+- [MongoDB Database (FREE)](https://www.mongodb.com/)
+
+## **Contributors**
+
+<a href="https://github.com/duckodas/fulldiscordbot/graphs/contributors">
+  <img src="https://stg.contrib.rocks/image?repo=duckodas/fulldiscordbot" />
+</a>
---
+++ index.js
@@ -0,0 +1,80 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+
+const { loadEvents } = require("./Handlers/eventHandler");
+const { loadCommands } = require("./Handlers/commandHandler");
+const { loadModals } = require("./Handlers/modalHandler");
+const { loadButtons } = require("./Handlers/buttonHandler");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User,
+  ],
+});
+
+client.config = require("./config.json");
+client.cooldowns = new Collection();
+client.commands = new Collection();
+client.events = new Collection();
+client.modals = new Collection();
+client.buttons = new Collection();
+
+loadEvents(client);
+client
+  .login(client.config.token)
+  .then(() => {
+    loadCommands(client);
+    loadModals(client);
+    loadButtons(client);
+  })
+  .catch((err) => console.log(err));
+
+
+process.on("unhandledRejection", (reason, promise) => {
+      console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+      console.log(reason, "\n", promise);
+    });
+    process.on("uncaughtException", (err, origin) => {
+      console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+      console.log(err, "\n", origin);
+    });
+    process.on("uncaughtExceptionMonitor", (err, origin) => {
+      console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+      console.log(err, "\n", origin);
+    });
+    process.on("multipleResolves", (type, promise, reason) => {
+      // console.log(" [Error_Handling] :: Multiple Resolves");
+      // console.log(type, promise, reason);
+      // console.log(reason);
+    });
